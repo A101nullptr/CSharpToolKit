93cc72c8559d1fddcac46839670a374c4de58fb4
A101nullptr <ahmed_hafez93@hotmail.com>
2024-03-29 05:11:00 +0100

    UIManager & base update

--- NotificationManager/Email.cs
+++ NotificationManager/Email.cs
@@ -7,49 +7,59 @@ using CSharpToolKit.TimeManager;
 
 namespace CSharpToolKit.NotificationManager
 {
+    /// <summary>
+    /// Provides functionality to send emails using SMTP protocol.
+    /// </summary>
     public class Email : IDisposable
     {
-        private readonly SmtpClient _smtpClient;
+        private SmtpClient _smtpClient { get; set; }
         private Log _log { get; set; }
         private Now _now { get; }
+        private string _email { get; set; }
 
-        public Email(string senderEmail, string senderPassword, string logPath)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Email"/> class with the specified sender email and password.
+        /// </summary>
+        /// <param name="senderEmail">The sender's email address.</param>
+        /// <param name="senderPassword">The password for the sender's email account.</param>
+        /// <param name="logPath">Optional. The path to the log file. If provided, logs will be recorded.</param>
+        public Email(string senderEmail, string senderPassword, string logPath = null)
         {
-            _smtpClient = new SmtpClient("smtp-mail.outlook.com", 587)
-            {
-                EnableSsl = true,
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(senderEmail, senderPassword)
-            };
+            _email = senderEmail;
+            EmailCheck(senderPassword);
 
-            _log = new Log(logPath); // Instantiates a logger with specified log path
-            _log.CreateLog();
-            
+            if (logPath != null)
+            {
+                _log = new Log(logPath);
+                _log.CreateLog();
+            }
+
             _now = new Now();
         }
 
-        public void SendEmailSync(string recipientEmail, string subject, string body)
+        /// <summary>
+        /// Checks the validity of the email address and initializes the SMTP client with the provided credentials.
+        /// </summary>
+        /// <param name="senderPassword">The password for the sender's email account.</param>
+        private void EmailCheck(string senderPassword)
         {
-            try
-            {
-                using (MailMessage mailMessage = new MailMessage())
-                {
-                    mailMessage.From = new MailAddress(((NetworkCredential)_smtpClient.Credentials).UserName);
-                    mailMessage.To.Add(recipientEmail);
-                    mailMessage.Subject = subject;
-                    mailMessage.Body = body;
+            var host = _email.Split('@')[1];
 
-                    _smtpClient.Send(mailMessage);
-                }
-
-                _log.WriteToLog($"NotificationManager::Email sent successfully on {_now.Date(0)} at {_now.Time(0)}");
-            }
-            catch (Exception ex)
+            _smtpClient = new SmtpClient($"smtp-mail.{host}", 587)
             {
-                _log.WriteToLog($"NotificationManager::Failed to send email:{ex.Message} on {_now.Date(0)} at {_now.Time(0)}");
-            }
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(_email, senderPassword)
+            };
         }
 
+        /// <summary>
+        /// Sends an email asynchronously.
+        /// </summary>
+        /// <param name="recipientEmail">The recipient's email address.</param>
+        /// <param name="subject">The subject of the email.</param>
+        /// <param name="body">The body of the email.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         public async Task SendEmailAsync(string recipientEmail, string subject, string body)
         {
             try
@@ -64,14 +74,51 @@ namespace CSharpToolKit.NotificationManager
                     await _smtpClient.SendMailAsync(mailMessage);
                 }
 
-                _log.WriteToLog($"NotificationManager::Email sent successfully on {_now.Date(0)} at {_now.Time(0)}");
+                MsgRecorder(1); 
             }
             catch (Exception ex)
             {
-                _log.WriteToLog($"NotificationManager::Failed to send email:{ex.Message} on {_now.Date(0)} at {_now.Time(0)}");
+                MsgRecorder(0, ex);
             }
         }
 
+        /// <summary>
+        /// Records a message indicating the outcome of the email sending operation.
+        /// </summary>
+        /// <param name="id">An identifier indicating the outcome. 0 for failure, 1 for success.</param>
+        /// <param name="ex">Optional. The exception encountered during the operation.</param>
+        private void MsgRecorder(int id, Exception ex = null)
+        {
+            string message;
+
+            if (id == 0)
+            {
+                message = $"NotificationManager::Failed to send email:{ex?.Message ?? "Unknown error"}";
+            }
+            else if (id == 1)
+            {
+                message = "NotificationManager::Email sent successfully";
+            }
+            else
+            {
+                throw new ArgumentException("Invalid id value", nameof(id));
+            }
+
+            message += $" on {_now.Date(0)} at {_now.Time(0)}";
+
+            if (_log != null)
+            {
+                _log.WriteToLog(message);
+            }
+            else
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        /// <summary>
+        /// Releases all resources used by the <see cref="Email"/> object.
+        /// </summary>
         public void Dispose()
         {
             _smtpClient.Dispose();
---
+++ UIManager/Progressbar.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CSharpToolKit.UIManager
+{
+    /// <summary>
+    /// Represents a custom progress bar window for displaying progress of a task.
+    /// </summary>
+    public partial class Progressbar : Window, IDisposable
+    {
+        /// <summary>
+        /// Gets a value indicating whether the progress bar window is closed.
+        /// </summary>
+        public bool IsClosed { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Progressbar"/> class.
+        /// </summary>
+        /// <param name="title">Optional. The title of the progress bar window.</param>
+        /// <param name="maximum">Optional. The maximum value of the progress bar.</param>
+        public Progressbar(string title = "", double maximum = 100.0)
+        {
+            InitializeComponent();
+            InitializeSize();
+            UpdateTitle(title);
+            processLabel.Content = title;
+            this.progressBar.Maximum = maximum;
+            this.Closed += (s, e) =>
+            {
+                IsClosed = true;
+            };
+        }
+
+        /// <summary>
+        /// Initializes the size and position of the progress bar window.
+        /// </summary>
+        private void InitializeSize()
+        {
+            this.SizeToContent = SizeToContent.WidthAndHeight;
+            this.Topmost = true;
+            this.ShowInTaskbar = false;
+            this.ResizeMode = ResizeMode.NoResize;
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        /// <summary>
+        /// Updates the title of the progress bar window.
+        /// </summary>
+        /// <param name="title">The new title of the progress bar window.</param>
+        public void UpdateTitle(string title)
+        {
+            this.Title = title;
+            processLabel.Content = title;
+        }
+
+        /// <summary>
+        /// Handles the left mouse button down event to enable window dragging.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The mouse button event arguments.</param>
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+                return;
+
+            DragMove();
+
+        }
+
+        /// <summary>
+        /// Updates the progress bar value.
+        /// </summary>
+        /// <param name="value">Optional. The value to increment the progress bar by.</param>
+        /// <returns>True if the progress bar window is closed, otherwise false.</returns>
+        public bool Update(double value = 1.0)
+        {
+            DoEvents();
+            progressBar.Value += value;
+            return IsClosed;
+        }
+
+        /// <summary>
+        /// Executes any pending events in the application's message queue.
+        /// </summary>
+        private void DoEvents()
+        {
+            System.Windows.Forms.Application.DoEvents();
+            System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.WaitCursor;
+        }
+
+        /// <summary>
+        /// Disposes of the progress bar window.
+        /// </summary>
+        public void Dispose()
+        {
+            if (!IsClosed) Close();
+        }
+    }
+}
